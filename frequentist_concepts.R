b85c76097d296728179d44b599d816e0bf90080d
arelativist <70415589+arelativist@users.noreply.github.com>
2020-10-15 09:20:54 -0400

    week_8

---
+++ frequentist_concepts.R
@@ -0,0 +1,18 @@
+# Question 1
+dbinom(3, 4, 0.75)
+
+#Question 2
+pbinom(3,4,0.75)
+
+#Question 3
+1 - pbinom(3,5,0.75)
+
+#Question 4
+pnorm(1.2, mean = 2, sd = 2)
+
+#Question 5
+1 - pnorm(1.2, mean = 2, sd =2)
+
+#Question 6
+
+pnorm(3.2, mean = 2, sd = 2) - pnorm(1.2, mean = 2, sd = 2)
---
+++ in_class_assignments/week_08_group.R
@@ -0,0 +1,24 @@
+## Load Data
+dat_bird = read.csv(here::here("data", "bird.sta.csv"))
+dat_habitat = read.csv(here::here("data", "hab.sta.csv"))
+dat_all = merge(dat_bird, dat_habitat)
+
+## Q 1
+
+wiwa_counts = c(2, 6)
+dpois(x = wiwa_counts, lambda = 1)
+sum(log(dpois(x = wiwa_counts, lambda = 4)))
+
+## lambda of 4 maximizes log likelihood
+
+
+## Q 2
+
+hist(dat_all$WIWR, breaks = 0:7 - 0.5)
+summary(dat_all$WIWR)
+sum(log(dpois(x = dat_all$WIWR, lambda = 1.0)))
+
+hist(dpois(x=dat_all$WIWR, lambda = 1.5))
+
+## Q 3 
+
